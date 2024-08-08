2ba41a5a3eb7d02dc9ca019b59d5f7a93d3a366b
Otávio Chaves Silva <1441383@sga.pucminas.br>
2024-08-07 21:59:33 -0300

    ..

--- LPM_otavio/src/App.java
+++ LPM_otavio/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Show de bola ");
+        System.out.println("Show de bosla ");
     }
 }
