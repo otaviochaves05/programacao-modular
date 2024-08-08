97190fa2931e80a9efe94bb9d21b08d1199eb44f
Otávio Chaves Silva <otaviocs2005@gmail.com>
2024-08-07 22:21:00 -0300

    .

--- LPM_otavio/src/App.java
+++ LPM_otavio/src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Show de bosla ");
+        
+        Scanner teclado = new Scanner(System.in);
+        int numero;
+        int unidade;
+        int dezena;
+        int meio;
+
+        numero = lerNumero(teclado);
+
+        unidade = numero % 10;
+        dezena = numero / 10;
+        meio = unidade + dezena;
+
+        teclado.close();
+
+    }
+
+    public static int lerNumero(Scanner teclado){
+        System.out.println("Digite um numero: ");
+        return Integer.parseInt(teclado.nextLine());
     }
 }
