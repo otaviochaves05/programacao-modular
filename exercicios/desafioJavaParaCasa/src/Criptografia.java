28ae5a8f8fae5c575da75e54ff4e65739491e53a
Otavio Chaves <otaviocs2005@gmail.com>
2024-08-13 16:35:08 -0300

    iniciando Criptografia.java

---
+++ exercicios/desafioJavaParaCasa/src/Criptografia.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.io.File;
+
+
+
+public class Criptografia {
+    public static void main(String[] args) {
+        
+        final String NOME_ARQUIVO = "criptografia.txt";
+        String[] respostaArquivo = lerArquivo(NOME_ARQUIVO);
+        
+    }
+
+    public static String[] lerArquivo(String NOME_ARQUIVO){ 
+
+        Scanner arquivo = new Scanner(new File(NOME_ARQUIVO));
+        
+        return
+    }
+
+}
